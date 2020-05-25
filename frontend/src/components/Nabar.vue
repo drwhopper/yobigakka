e6b73f63d717a88d4ef38151b8f4ac2734ab55b1
whopper <whopperisdelicious@gmail.com>
2020-05-26 00:54:47 +0900

    introduce buefy

---
+++ frontend/src/components/Nabar.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav class="navbar is-light">
+    <div class="navbar-brand">
+      <span class="navbar-item">
+        予備学科
+      </span>
+    </div>
+  </nav>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class Nabar extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,8 +1,10 @@
 import Vue from "vue";
 import App from "./App.vue";
+import Buefy from "buefy";
 import router from "./router";
 
 Vue.config.productionTip = false;
+Vue.use(Buefy);
 
 new Vue({
   router,
