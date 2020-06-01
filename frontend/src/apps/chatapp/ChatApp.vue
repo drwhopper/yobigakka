b98ad8cb696e84e0a702f574931629b03bb439d7
drwhopper <whopperisdelicious@gmail.com>
2020-06-01 23:32:22 +0900

    send message to websocket chat

--- frontend/src/apps/chatapp/ChatApp.vue
+++ frontend/src/apps/chatapp/ChatApp.vue
@@ -1,12 +1,44 @@
 <template>
   <div id="chatapp">
     <Nabar />
-    <HelloWorld msg="ちゃちゃっと" />
-    <div id="chat">
-      <div id="messages" v-for="message in messages" :key="message">
-        <div id="message">{{ message }}</div>
+    <section class="hero is-bold is-primary">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">ChatRoom</h1>
+          <h2>0101</h2>
+        </div>
       </div>
-    </div>
+    </section>
+    <section class="section">
+      <div class="columns">
+        <div class="column is-3">
+          <div class="box">
+            <p class="title">Cards</p>
+          </div>
+        </div>
+        <div class="column is-7">
+          <section>
+            <b-field label="会話" grouped>
+              <b-input
+                placeholder="ノンストップ議論"
+                expanded
+                v-model="myMessage"
+              >
+              </b-input>
+              <b-button class="button is-info" v-on:click="send()">
+                発言
+              </b-button>
+            </b-field>
+          </section>
+          <TalkView v-bind:chatmsg="messages" />
+        </div>
+        <div class="column is-2">
+          <div class="box">
+            <p class="title">Members</p>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -14,22 +46,33 @@
 import { Component, Vue } from "vue-property-decorator";
 import HelloWorld from "@/components/HelloWorld.vue";
 import Nabar from "@/components/Nabar.vue";
+import TalkView from "@/components/chat/TalkView.vue";
 
 @Component({
   components: {
     HelloWorld,
-    Nabar
+    Nabar,
+    TalkView
   }
 })
 export default class ChatApp extends Vue {
-  messages: string[] = [];
-  websocket: WebSocket | null = null;
+  private messages: string[] = [];
+  private myMessage = "";
+  private websocket!: WebSocket;
   mounted() {
     this.websocket = new WebSocket("ws://localhost:19000/chat");
     this.websocket.onmessage = this.onMessage;
   }
   onMessage(event: MessageEvent) {
     this.messages.push(event.data);
+  }
+  send() {
+    const text = this.myMessage && this.myMessage.trim();
+    if (!text) {
+      return;
+    }
+    this.websocket.send(this.myMessage);
+    this.myMessage = "";
   }
 }
 </script>
---
+++ frontend/src/components/chat/TalkView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="chat">
+    <div id="messages" v-for="message in chatmsg" :key="message">
+      <div class="box" id="message">{{ message }}</div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class TalkView extends Vue {
+  @Prop() private chatmsg!: string[];
+}
+</script>
